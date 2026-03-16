d02fe0e8eb4a5d3d08834bfcf231e36591e488f6
MichaelJDani <Ozordanielmichael@gmail.com>
2026-03-16 11:21:22 +0100

    my local changes

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import configuration from './env.js';
 
 export const corsOptions = {
-  origin: ['http://localhost:3000'], // frontend URL
+  origin: ['http://localhost:3000', "http://127.0.0.1:3000"], // frontend URL
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
